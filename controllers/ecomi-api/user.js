30d871ada1f576ea33612ab60c9a5aa87048fa5e
LK <lk@alienbuild.uk>
2021-05-15 14:45:16 +0100

    Update ALICE routes

--- controllers/ecomi-api/user.js
+++ controllers/ecomi-api/user.js
@@ -2,63 +2,15 @@ require('isomorphic-fetch');
 require('dotenv').config()
 
 exports.read = (req,res) => {
-
     const userId = req.params.slug
-
-    const userQuery =`
-         query UserLookUp{
-            user(id:"${userId}"){
-                id
-                profileVisible
-                totalCollectibles
-                profileCollectibles{
-                    edges{
-                        node{
-                            id
-                            ownedByUser
-                            formattedIssueNumber
-                            collectibleType {
-                                id
-                                rarity
-                                totalAvailable
-                                totalIssued
-                                storePrice
-                                storeCurrencyType
-                                name
-                                image{
-                                    id
-                                    url
-                                    direction
-                                }
-                            }
-                        }
-                    }
-                }
-                username
-                avatar {
-                    url
-                    id
-                }
-                totalLikes
-                totalFollowers
-                totalFullSets
-                collectorStatus
-                bio
-            }   
-     }
-    `
-
-    fetch('https://api.prod.veve.me/graphql', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'cookie': process.env.ECOMI_TOKEN
-        },
-        body: JSON.stringify({ query: userQuery}),
-    })
-        .then(data => data.json())
+    fetch(`${process.env.ALICE}ecomi/users/${userId}`)
+        .then(response => {
+            if (response.status >= 400) {
+                throw new Error("Bad response from server");
+            }
+            return response.json();
+        })
         .then(data => {
             res.json(data.data.user)
         })
-        .catch(e => console.log('Error: ', e))
 }
