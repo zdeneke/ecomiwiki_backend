e85e4e476dc0f12603ab5daacf3cad56d1b66d37
LK <lk@alienbuild.uk>
2021-08-17 09:49:17 +0100

    End of day commit

--- helpers/index.js
+++ helpers/index.js
@@ -1,4 +1,5 @@
 exports.getPercentageChangeNumberOnly = (currentNumber, prevNumber) => {
+    console.log(`Current number is: ${currentNumber} and prev is ${prevNumber}`)
     const calc = (currentNumber - prevNumber) / prevNumber * 100
     return calc
 }
