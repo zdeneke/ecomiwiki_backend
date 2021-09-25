3e842da35f7c480b6cfa087bdaba1a55d291a9b9
LK <lk@alienbuild.uk>
2021-09-25 19:30:51 +0100

    Update comic data

--- models/metrics/ComicPrice.js
+++ models/metrics/ComicPrice.js
@@ -1,18 +1,20 @@
 const mongoose = require('mongoose')
 
 const ComicPrice = new mongoose.Schema({
+    uniqueCoverId: {
+        type: String
+    },
     comicId: {
         type: String,
     },
-    coverId: {
+    name: {
         type: String
     },
     comicSeries: {
-        name: {
-            type: String,
-            required: true
+        id:{
+            type: String
         },
-        description: {
+        name: {
             type: String
         }
     },
@@ -33,67 +35,32 @@ const ComicPrice = new mongoose.Schema({
             thumbnailUrl: {
                 type: String
             },
-            lowResolutionUrl: {
-                type: String
-            },
-            medResolutionUrl: {
-                type: String
-            },
-            highResolutionUrl: {
-                type: String
-            },
-            fullResolutionUrl: {
-                type: String
-            },
             direction: {
                 type: String
             }
         }
     },
-    slug: {
-        type: String
-    },
     totalIssued: {
         type: Number
     },
-    storePrice: {
+    totalAvailable: {
         type: Number
     },
-    pageCount: {
+    storePrice: {
         type: Number
     },
     comicNumber: {
         type: Number
     },
     metrics:{
-        issueNumber: {
-            type: Number
-        },
         lowestPrice: {
             type: Number
         },
         totalListings: {
             type: Number
         },
-        createdAt: {
-            type: Date
-        },
         updatedAt: {
             type: Date
-        },
-        prevSold: {
-            "price": {
-                type: Number
-            },
-            "createdAt": {
-                type: Date
-            },
-            "issueNumber": {
-                type: Number
-            },
-            "listingType": {
-                type: String
-            }
         },
     }
 }, { timestamps: true })
