483f0307725107dfbde8a227ac242704417add6b
LK <lk@alienbuild.uk>
2021-11-24 10:15:31 +0000

    Disable http to https redirect

--- server.js
+++ server.js
@@ -50,10 +50,10 @@ const corsOptions = {
     optionsSuccessStatus: 200
 }
 
-app.enable('trust proxy')
-app.use((req, res, next) => {
-    req.secure ? next() : res.redirect('https://' + req.headers.host + req.url)
-})
+// app.enable('trust proxy')
+// app.use((req, res, next) => {
+//     req.secure ? next() : res.redirect('https://' + req.headers.host + req.url)
+// })
 
 app.use(cors(corsOptions))
 app.disable('etag');
